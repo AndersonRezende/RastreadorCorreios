c5648f4b3420679ae623a2db5a1b46f3fd5b195b
Anderson Rezende <andersonrezende17@hotmail.com>
2021-09-06 10:51:33 -0300

    Adiciona rastreador dos correios

---
+++ rastrear.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ]
+  then
+  	echo "Insira o código de rastreio:"
+	read codigo
+  else
+  	codigo=$1
+fi
+
+echo "Rastreando "$codigo
+
+response=$(curl -X GET -G 'https://rastreamento.correios.com.br/app/resultado.php' \
+-d objeto=$codigo \
+-d mqs=S)
+
+descricao=$( jq -r '.eventos[0].descricao' <<< "${response}" )
+origem=$( jq -r '.eventos[0].unidade.nome' <<< "${response}" )
+destino=$( jq -r '.eventos[0].unidadeDestino.nome' <<< "${response}" )
+data=$( jq -r '.eventos[0].dtHrCriado' <<< "${response}" )
+echo ${descricao}
+echo ${origem} " -> " ${destino}
+echo $data
